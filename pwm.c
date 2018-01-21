3c4a7eb8d8807bc42fab654a61717a904b3769ef
David Case <Dav3xor@gmail.com>
2018-01-20 23:34:06 -0800

    finished writing next_rising_edge function

--- pwm.c
+++ pwm.c
@@ -9,4 +9,5 @@ unsigned int cur_time = 0;
 unsigned int last_rise = 0;
 
 unsigned int next_rising_edge(void) {
-  return last_rise + (freq/rate) + sin(fmod(freq, last_rise
+  return last_rise + ((freq/rate) + sin(fmod(freq, last_rise)*TAU)*(freq/rate));
+}
