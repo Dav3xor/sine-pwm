3025f29acb27ae3dd59b1957fd85c63e44bc6072
David Case <Dav3xor@gmail.com>
2018-01-20 22:58:18 -0800

    first commit

---
+++ pwm.c
@@ -0,0 +1,12 @@
+#define TAU 3.14159*2
+
+float freq;     // time units per full TAU cycle
+float rate;     // number of rising edges per TAU
+float width;
+float phase;
+
+unsigned int cur_time = 0;
+unsigned int last_rise = 0;
+
+unsigned int next_rising_edge(void) {
+  return last_rise + (freq/rate) + sin(fmod(freq, last_rise
