39cc444cf9bed50f954fb83086012b50cdc3546e
David Case <Dav3xor@gmail.com>
2018-01-21 00:53:39 -0800

    next_rising_edge now works

--- pwm.c
+++ pwm.c
@@ -1,13 +1,25 @@
-#define TAU 3.14159*2
+#include <math.h>
+#include <stdio.h>
 
-float freq;     // time units per full TAU cycle
-float rate;     // number of rising edges per TAU
+#define TAU (3.14159*2)
+
+float freq=500.0;     // time units per full TAU cycle
+float rate=20.0;     // number of rising edges per TAU
 float width;
 float phase;
 
 unsigned int cur_time = 0;
 unsigned int last_rise = 0;
 
-unsigned int next_rising_edge(void) {
-  return last_rise + ((freq/rate) + sin(fmod(freq, last_rise)*TAU)*(freq/rate));
+float next_rising_edge(void) {
+  return last_rise + ((freq/rate) + (sin((last_rise/freq)*TAU)*(freq/rate)));
+}
+
+
+int main(void) {
+  for(int i=0;i<500;i++) {
+    printf("prev = %d next = %f\n", last_rise, next_rising_edge());
+    last_rise+=1;
+  }
+  printf("%f\n", last_rise/freq);
 }
