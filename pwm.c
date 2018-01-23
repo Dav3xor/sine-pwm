a61cf9e4645a41505329cb9d5a6379d8438b2a71
David Case <Dav3xor@gmail.com>
2018-01-22 23:29:51 -0800

    added phase

--- pwm.c
+++ pwm.c
@@ -3,20 +3,20 @@
 
 #define TAU (3.14159*2)
 
-float freq      = 500.0;     // time units per full TAU cycle
-float rate      = 20.0;     // number of rising edges per TAU
-float duty_cycle= 0.5;   // 0.0-1.0, percentage
-float phase;
+float freq       = 500.0;     // time units per full TAU cycle
+float rate       = 20.0;     // number of rising edges per TAU
+float duty_cycle = 0.5;   // 0.0-1.0, percentage
+float phase      = 0.0;
 
 unsigned int cur_time = 0;
 unsigned int last_rise = 0;
 
 float next_rising_edge(void) {
-  return last_rise + ((freq/rate) + (sin((last_rise/freq)*TAU)*(freq/rate)));
+  return last_rise + ((freq/rate) + (sin((last_rise/freq+phase)*TAU)*(freq/rate)));
 }
 
 float next_falling_edge(void) {
-  return last_rise + (((freq/rate) + (sin((last_rise/freq)*TAU)*(freq/rate)))*pulse_width);
+  return last_rise + (((freq/rate) + (sin((last_rise/freq)*TAU+phase)*(freq/rate)))*duty_cycle);
 }
 
 int main(void) {
