6da7ae300b79fc99afb2183470a573a60249ac5b
David Case <Dav3xor@gmail.com>
2018-01-24 01:18:14 +0000

    more fiddling with rise/fall times

--- pwm.cpp
+++ pwm.cpp
@@ -18,36 +18,55 @@ class SinePWM {
       duty_cycle  = 0.5;
       phase       = 0.0;
       cur_time    = 0;
-      last_change = 0;
+      last_rising = 0;
       next_change = 0;
-      cur_state   = low;
+      cur_state   = high;
+    }
+
+
+    set_edges(void) {
+      next_rising  = next_rising + (freq/rate) + (sin((next_rising/freq)*TAU+phase)*(freq/rate));
+      next_falling = next_rising + ((freq/rate) + sin((last_rising/freq)*TAU+phase)*(freq/rate))*duty_cycle;
     }
 
     float next_rising_edge(void) {
-      return last_change + ((freq/rate) + (sin((last_change/freq)*TAU+phase)*(freq/rate)));
+      return last_rising + (freq/rate) + (sin((last_rising/freq)*TAU+phase)*(freq/rate));
     }
 
     float next_falling_edge(void) {
-      return last_change + (((freq/rate) + (sin((last_change/freq)*TAU+phase)*(freq/rate)))*duty_cycle);
+      return last_rising + (( (freq/rate) + (sin((last_rising/freq)*TAU+phase)*(freq/rate))*duty_cycle));
     }
 
     void tick(void) {
       cout << "tick" << endl;
       if (cur_time >= next_change) {
-        last_change = cur_time;
         if(cur_state == low) {
           cur_state = high;
-          next_change = next_rising_edge();
+          next_change = next_falling_edge();
+          last_rising = cur_time;
           cout << "rising edge " << next_change << endl;
         } else {
           cur_state = low;
-          next_change = next_falling_edge();
+          next_change = next_rising_edge();
           cout << "falling edge " << next_change << endl;
         }
       }
       cur_time += 1;
     }
-  
+   
+    void new_edge(void) {
+      if(cur_state==low) {
+        next_change = next_rising_edge();
+      } else {
+        next_change = next_falling_edge();
+      }
+    }
+
+    void set_freq(float new_freq) {
+      freq = new_freq;
+      new_edge();
+    }
+
   private:
     enum State { high, low };
     float freq;         // time units per full TAU cycle
@@ -55,7 +74,7 @@ class SinePWM {
     float duty_cycle;   // 0.0-1.0, percentage
     float phase;
     unsigned int cur_time;
-    unsigned int last_change;
+    unsigned int last_rising;
     unsigned int next_change;
     State cur_state;
 };
